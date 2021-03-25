318197bb0f4cea5bc45a2f0de86cc877f9db0445
LanceD18 <lancedugars16@gmail.com>
2021-03-25 15:59:08 -0500

    Implemented a minor fix for connecting to mongodb

--- index.ts
+++ index.ts
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 import {json} from "body-parser";
 import routes from "./routes";
 
-mongoose.connect('mongodb://127.0.0.1/ATCBackend'); 
+const dotenv = require("dotenv");
+dotenv.config();
+
+const uri = process.env.DB_URL || '127.0.0.1';
+mongoose.connect('mongodb://' + uri + '/ATCBackend').catch((err) => { console.log(err); process.exit(1); })
 
 express()
 .use(json())
