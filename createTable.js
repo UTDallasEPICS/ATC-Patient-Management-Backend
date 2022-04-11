03ce396394549c819f07c4be7b5fd2aa0e0c2f37
PretaxEnd <rjdurana@gmail.com>
2022-04-11 00:14:49 -0500

    sample database files for docker

--- createTable.js
+++ createTable.js
@@ -1,3 +1,22 @@
-import { index } from "typegoose";
-import models from models/index.ts
+//Importing Mongoose
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
+
+//Creating a database
+await mongoose.connect('mongodb://localhost/ATC-Database');
+
+//Creating Therapist Model
+const therapistSchema = new Schema({
+    firstName:{type: String},
+    lastName:{type: String},
+    email:{type: String},
+    username:{type: String, default: 'user'},
+    password:{type: String, default: 'password'},
+    phoneNumber:{type: String},
+    isAdmin:{type: Boolean}
+});
+
+const therapistModel = mongoose.model('therapists', therapistSchema);
+
+const therapistTable = new therapistModel();
--- dockerfile
+++ dockerfile
@@ -1,2 +1,9 @@
 FROM mongo:latest
 
+ENV MONGO_INITDB_ROOT_USERNAME: username 
+ENV MONGO_INITDB_ROOT_PASSWORD: password 
+WORKDIR /
+
+COPY createTable.js /docker-entrypoint-initdb.d/createTable.js
+
+EXPOSE 27107
